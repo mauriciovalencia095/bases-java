586c96776efde926b3bbae4a44dd8de37ef9bb85
mauriciovalencia095 <mauriciovalencia095@gmail.com>
2023-09-05 13:18:32 -0500

    Segundo commit

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,10 @@
-
 public class Main {
-    public static void main(String[] args) {
 
-        System.out.println("Hello world!");
+
+    public static void main(String[] args) {
+        String mensaje = "Hola mundo";
+        System.out.println(mensaje);
     }
+
+
 }
